f74ecdd204ab96af4bf15551e1c6aaf848b399b2
RamanShinde <ramanshinde064@gmail.com>
2025-08-02 18:59:35 +0530

    Updated AuthService with SecurityConfig, login fixes

--- AuthService/src/main/java/com/example/AuthService/Configuration/SecurityConfig.java
+++ AuthService/src/main/java/com/example/AuthService/Configuration/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.example.AuthService.Configuration;
 
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,16 +22,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/User/SingupUser", "/User/login").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
-
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("User/Message").permitAll()
+                .requestMatchers("User/SingupUser").permitAll()
+                .requestMatchers("User/login").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement().
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authmanager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
